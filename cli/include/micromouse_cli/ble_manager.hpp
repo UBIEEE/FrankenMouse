f6afafc967751cee7944d50072d10a5e72f296d7
Peter Lilley <hi.petelilley@gmail.com>
2025-05-11 01:15:18 -0400

    Implemented writing and receiving BLE characteristics

--- cli/include/micromouse_cli/ble_manager.hpp
+++ cli/include/micromouse_cli/ble_manager.hpp
@@ -2,13 +2,131 @@
 
 #include <simpleble/SimpleBLE.h>
 
+#include <micromouse_cli/diagnostics.hpp>
+
 #include <functional>
 #include <optional>
+#include <set>
+#include <span>
 #include <string>
 #include <string_view>
 
 #define DEFAULT_PERIPHERAL_NAME "PetersMicroMouse"
 #define DEFAULT_ADAPTER_INDEX 0
+
+enum BLEService {
+  SERVICE_UNKNOWN = -1,
+  SERVICE_MUSIC,
+  SERVICE_VISION,
+  SERVICE_MAIN,
+  SERVICE_DRIVE,
+  SERVICE_MAZE,
+  _SERVICE_COUNT,
+};
+
+enum BLECharacteristic {
+  CHAR_MUSIC_PLAY_SONG,
+  CHAR_MUSIC_IS_PLAYING,
+
+  CHAR_VISION_RAW_READINGS,
+  CHAR_VISION_DISTANCES,
+  CHAR_VISION_CALIBRATE,
+
+  CHAR_MAIN_TASK,
+  CHAR_MAIN_APP_READY,
+  CHAR_MAIN_ERROR_CODE,
+
+  CHAR_DRIVE_MOTOR_DATA,
+  CHAR_DRIVE_IMU_DATA,
+  CHAR_DRIVE_PID_CONSTANTS,
+
+  CHAR_MAZE_RESET,
+  CHAR_MAZE_CELL,
+
+  _CHAR_COUNT,
+};
+
+// Topics sent to the robot.
+enum class BLETopicWrite {
+  MAIN_TASK = CHAR_MAIN_TASK,
+  MAIN_APP_READY = CHAR_MAIN_APP_READY,
+
+  DRIVE_PID = CHAR_DRIVE_PID_CONSTANTS,
+  VISION_CALIBRATE = CHAR_VISION_CALIBRATE,
+
+  MAZE_RESET = CHAR_MAZE_RESET,
+
+  MUSIC_PLAY_SONG = CHAR_MUSIC_PLAY_SONG,
+};
+
+template <BLETopicWrite Topic, typename Default = void>
+struct BLETopicWriteData;
+
+template <>
+struct BLETopicWriteData<BLETopicWrite::DRIVE_PID> {
+  using type = float[3 + 3];
+};
+
+template <BLETopicWrite Topic>
+struct BLETopicWriteData<Topic, void> {
+  using type = uint8_t;
+};
+
+// Topics received from the robot.
+enum class BLETopicNotify {
+  MAIN_TASK = CHAR_MAIN_TASK,
+  MAIN_ERROR = CHAR_MAIN_ERROR_CODE,
+
+  DRIVE_MOTOR_DATA = CHAR_DRIVE_MOTOR_DATA,
+  DRIVE_IMU_DATA = CHAR_DRIVE_IMU_DATA,
+  DRIVE_PID = CHAR_DRIVE_PID_CONSTANTS,
+
+  VISION_RAW_READINGS = CHAR_VISION_RAW_READINGS,
+  VISION_DISTANCES = CHAR_VISION_DISTANCES,
+  VISION_CALIBRATE = CHAR_VISION_CALIBRATE,
+
+  MAZE_CELL = CHAR_MAZE_CELL,
+  // MAZE_MOUSE_POSITION = ,
+
+  MUSIC_IS_PLAYING = CHAR_MUSIC_IS_PLAYING,
+};
+
+template <BLETopicNotify Topic, typename Default = void>
+struct BLETopicNotifyData;
+
+template <>
+struct BLETopicNotifyData<BLETopicNotify::DRIVE_MOTOR_DATA> {
+  using type = float[4 + 3];
+};
+template <>
+struct BLETopicNotifyData<BLETopicNotify::DRIVE_IMU_DATA> {
+  using type = float[3 + 3];
+};
+template <>
+struct BLETopicNotifyData<BLETopicNotify::DRIVE_PID> {
+  using type = float[3 + 3];
+};
+template <>
+struct BLETopicNotifyData<BLETopicNotify::VISION_RAW_READINGS> {
+  using type = float[4];
+};
+template <>
+struct BLETopicNotifyData<BLETopicNotify::VISION_DISTANCES> {
+  using type = float[4];
+};
+template <>
+struct BLETopicNotifyData<BLETopicNotify::VISION_CALIBRATE> {
+  using type = bool;
+};
+template <>
+struct BLETopicNotifyData<BLETopicNotify::MUSIC_IS_PLAYING> {
+  using type = bool;
+};
+
+template <BLETopicNotify Topic>
+struct BLETopicNotifyData<Topic, void> {
+  using type = uint8_t;
+};
 
 class BLEManager final {
   std::string m_peripheral_search_name;
@@ -65,6 +183,80 @@ class BLEManager final {
    */
   void process_events();
 
+  //
+  // Write
+  //
+
+  template <BLETopicWrite Topic>
+  void write(const BLETopicWriteData<Topic>::type& value) {
+    using ValueType = BLETopicWriteData<Topic>::type;
+
+    const uint8_t* data = reinterpret_cast<const uint8_t*>(&value);
+    write(static_cast<BLECharacteristic>(Topic), data, sizeof(ValueType));
+  }
+
+  //
+  // Notify
+  //
+
+  struct MusicNotifyData {
+    bool is_playing = false;
+  };
+  const MusicNotifyData& music_data() const { return m_music_data; }
+
+  struct VisionNotifyData {
+    float raw_readings[4] = {0};
+    float distances[4] = {0};
+    bool is_calibrated = false;
+  };
+  const VisionNotifyData& vision_data() const { return m_vision_data; }
+
+  struct DriveNotifyData {
+    float motor_data[4 + 3] = {0};
+    float imu_data[3 + 3] = {0};
+    float pid_data[3 + 3] = {0};
+  };
+  const DriveNotifyData& drive_data() const { return m_drive_data; }
+
+  struct MainNotifyData {
+    uint8_t task = 0;
+    uint8_t error_code = 0;
+  };
+  const MainNotifyData& main_data() const { return m_main_data; }
+
+  struct MazeNotifyData {
+    uint8_t cell = 0;
+  };
+  const MazeNotifyData& maze_data() const { return m_maze_data; }
+
+  template <BLETopicNotify Topic>
+  const BLETopicNotifyData<Topic>::type& get_data() const {
+    using enum BLETopicNotify;
+
+    if constexpr (Topic == MAIN_TASK) {
+      return m_main_data.task;
+    } else if constexpr (Topic == MAIN_ERROR) {
+      return m_main_data.error_code;
+    } else if constexpr (Topic == DRIVE_MOTOR_DATA) {
+      return m_drive_data.motor_data;
+    } else if constexpr (Topic == DRIVE_IMU_DATA) {
+      return m_drive_data.imu_data;
+    } else if constexpr (Topic == DRIVE_PID) {
+      return m_drive_data.pid_data;
+    } else if constexpr (Topic == VISION_RAW_READINGS) {
+      return m_vision_data.raw_readings;
+    } else if constexpr (Topic == VISION_DISTANCES) {
+      return m_vision_data.distances;
+    } else if constexpr (Topic == VISION_CALIBRATE) {
+      return m_vision_data.is_calibrated;
+    } else if constexpr (Topic == MAZE_CELL) {
+      return m_maze_data.cell;
+    } else if constexpr (Topic == MUSIC_IS_PLAYING) {
+      return m_music_data.is_playing;
+    } else
+      static_assert(false, "Unsupported notify type");
+  }
+
  private:
   bool initialize_adapter();
   void configure_scan_callbacks();
@@ -75,4 +267,60 @@ class BLEManager final {
   void on_scan_found(SimpleBLE::Peripheral peripheral);
   void on_connected();
   void on_disconnected();
+
+  void configure_peripheral_notifications();
+
+ private:
+  /**
+   * @brief Return the service for which the characteristic belongs to.
+   */
+  static BLEService get_characteristic_service(BLECharacteristic char_id);
+
+  /**
+   * @brief Write to a characteristic.
+   *
+   * @param char_id ID of the characteristic to write to.
+   * @param data    Pointer to the data to write.
+   * @param size    Number of bytes to write.
+   */
+  void write(BLECharacteristic char_id, const uint8_t* data, size_t size);
+
+  using NotificationFunc = std::function<void(SimpleBLE::ByteArray payload)>;
+
+  /**
+   * @brief Register a callback for a characteristic notification.
+   *
+   * @param char_id  ID of the notify characteristic.
+   * @param callback Callback function called when a notification is received.
+   */
+  void notify(BLECharacteristic char_id, NotificationFunc callback);
+
+  template <BLETopicNotify Topic>
+  NotificationFunc make_notification_callback(
+      BLETopicNotifyData<Topic>::type& data) {
+    return [&](SimpleBLE::ByteArray payload) {
+      using ValueType = BLETopicNotifyData<Topic>::type;
+      constexpr size_t ValueSize = sizeof(ValueType);
+      if (payload.size() != ValueSize) {
+        report_warning(name(), "notification for topic %d has invalid size %zu",
+                       static_cast<int>(Topic), payload.size());
+        return;
+      }
+
+      (void)std::memcpy(&data, payload.data(), ValueSize);
+    };
+  }
+
+  template <BLETopicNotify Topic>
+  void notify(BLETopicNotifyData<Topic>::type& data) {
+    notify(static_cast<BLECharacteristic>(Topic),
+           make_notification_callback<Topic>(data));
+  }
+
+ private:
+  MusicNotifyData m_music_data;
+  DriveNotifyData m_drive_data;
+  VisionNotifyData m_vision_data;
+  MainNotifyData m_main_data;
+  MazeNotifyData m_maze_data;
 };
--- cli/src/ble_manager.cpp
+++ cli/src/ble_manager.cpp
@@ -1,13 +1,49 @@
 #include <micromouse_cli/ble_manager.hpp>
 
-#include <micromouse_cli/diagnostics.hpp>
+#include <cassert>
 
 using namespace std::placeholders;
+using namespace std::string_literals;
 
-BLEManager::BLEManager(std::string_view peripheral_name, int adapter_index, bool dummy)
+static const SimpleBLE::BluetoothUUID s_services[_SERVICE_COUNT] = {
+    [SERVICE_MUSIC] = "00000000-cc7a-482a-984a-7f2ed5b3e58f"s,
+    [SERVICE_VISION] = "00000001-cc7a-482a-984a-7f2ed5b3e58f"s,
+    [SERVICE_MAIN] = "00000002-cc7a-482a-984a-7f2ed5b3e58f"s,
+    [SERVICE_DRIVE] = "00000003-cc7a-482a-984a-7f2ed5b3e58f"s,
+    [SERVICE_MAZE] = "00000004-cc7a-482a-984a-7f2ed5b3e58f"s,
+};
+
+static const SimpleBLE::BluetoothUUID s_characteristics[_CHAR_COUNT] = {
+    // Music (0000-0004)
+    [CHAR_MUSIC_PLAY_SONG] = "00000000-8e22-4541-9d4c-21edae82ed19"s,
+    [CHAR_MUSIC_IS_PLAYING] = "00000001-8e22-4541-9d4c-21edae82ed19"s,
+
+    // Vision (0005-0009)
+    [CHAR_VISION_RAW_READINGS] = "00000005-8e22-4541-9d4c-21edae82ed19"s,
+    [CHAR_VISION_DISTANCES] = "00000006-8e22-4541-9d4c-21edae82ed19"s,
+    [CHAR_VISION_CALIBRATE] = "00000007-8e22-4541-9d4c-21edae82ed19"s,
+
+    // Main (000A-000E)
+    [CHAR_MAIN_TASK] = "0000000a-8e22-4541-9d4c-21edae82ed19"s,
+    [CHAR_MAIN_APP_READY] = "0000000b-8e22-4541-9d4c-21edae82ed19"s,
+    [CHAR_MAIN_ERROR_CODE] = "0000000c-8e22-4541-9d4c-21edae82ed19"s,
+
+    // Drive (000F-0013)
+    [CHAR_DRIVE_MOTOR_DATA] = "0000000f-8e22-4541-9d4c-21edae82ed19"s,
+    [CHAR_DRIVE_IMU_DATA] = "00000010-8e22-4541-9d4c-21edae82ed19"s,
+    [CHAR_DRIVE_PID_CONSTANTS] = "00000011-8e22-4541-9d4c-21edae82ed19"s,
+
+    // Maze (0014-0018)
+    [CHAR_MAZE_RESET] = "00000014-8e22-4541-9d4c-21edae82ed19"s,
+    [CHAR_MAZE_CELL] = "00000015-8e22-4541-9d4c-21edae82ed19"s,
+};
+
+BLEManager::BLEManager(std::string_view peripheral_name,
+                       int adapter_index,
+                       bool dummy)
     : m_peripheral_search_name(peripheral_name),
-      m_adapter_index(adapter_index), m_dummy(dummy) {
-
+      m_adapter_index(adapter_index),
+      m_dummy(dummy) {
   if (m_dummy) {
     m_initialized = true;
     return;
@@ -136,6 +172,9 @@ void BLEManager::on_connected() {
   if (m_on_connect_callback)
     m_on_connect_callback();
 
+  configure_peripheral_notifications();
+  write<BLETopicWrite::MAIN_APP_READY>(1);
+
   report_status(name(), "connected to '%s'", m_peripheral_search_name.c_str());
 }
 
@@ -148,4 +187,90 @@ void BLEManager::on_disconnected() {
 
   m_peripheral = std::nullopt;
   begin_scan();
+}
+
+void BLEManager::configure_peripheral_notifications() {
+  if (!is_connected())
+    return;
+
+  notify<BLETopicNotify::MAIN_TASK>(m_main_data.task);
+  notify<BLETopicNotify::MAIN_ERROR>(m_main_data.error_code);
+
+  notify<BLETopicNotify::DRIVE_MOTOR_DATA>(m_drive_data.motor_data);
+  notify<BLETopicNotify::DRIVE_IMU_DATA>(m_drive_data.imu_data);
+  notify<BLETopicNotify::DRIVE_PID>(m_drive_data.pid_data);
+
+  notify<BLETopicNotify::VISION_RAW_READINGS>(m_vision_data.raw_readings);
+  notify<BLETopicNotify::VISION_DISTANCES>(m_vision_data.distances);
+  notify<BLETopicNotify::VISION_CALIBRATE>(m_vision_data.is_calibrated);
+
+  notify<BLETopicNotify::MAZE_CELL>(m_maze_data.cell);
+
+  notify<BLETopicNotify::MUSIC_IS_PLAYING>(m_music_data.is_playing);
+}
+
+BLEService BLEManager::get_characteristic_service(BLECharacteristic char_id) {
+  switch (char_id) {
+    case CHAR_MUSIC_PLAY_SONG:
+    case CHAR_MUSIC_IS_PLAYING:
+      return SERVICE_MUSIC;
+    case CHAR_VISION_RAW_READINGS:
+    case CHAR_VISION_DISTANCES:
+    case CHAR_VISION_CALIBRATE:
+      return SERVICE_VISION;
+    case CHAR_MAIN_TASK:
+    case CHAR_MAIN_APP_READY:
+    case CHAR_MAIN_ERROR_CODE:
+      return SERVICE_MAIN;
+    case CHAR_DRIVE_MOTOR_DATA:
+    case CHAR_DRIVE_IMU_DATA:
+    case CHAR_DRIVE_PID_CONSTANTS:
+      return SERVICE_DRIVE;
+    case CHAR_MAZE_RESET:
+    case CHAR_MAZE_CELL:
+      return SERVICE_MAZE;
+    default:
+      assert(false);
+      return SERVICE_UNKNOWN;
+  }
+}
+
+void BLEManager::write(BLECharacteristic char_id,
+                       const uint8_t* data,
+                       size_t size) {
+  if (m_dummy)
+    return;
+  if (!is_connected())
+    return;
+
+  BLEService service_id = get_characteristic_service(char_id);
+  assert(service_id != SERVICE_UNKNOWN);
+
+  SimpleBLE::BluetoothUUID service_uuid = s_services[service_id];
+  SimpleBLE::BluetoothUUID char_uuid = s_characteristics[char_id];
+
+  bool result = m_peripheral->write_request(service_uuid, char_uuid,
+                                            SimpleBLE::ByteArray(data, size));
+  if (!result) {
+    report_error(name(), "failed to publish characteristic %d", char_id);
+  }
+}
+
+void BLEManager::notify(BLECharacteristic char_id, NotificationFunc callback) {
+  if (m_dummy)
+    return;
+  if (!is_connected())
+    return;
+
+  BLEService service_id = get_characteristic_service(char_id);
+  assert(service_id != SERVICE_UNKNOWN);
+
+  SimpleBLE::BluetoothUUID service_uuid = s_services[service_id];
+  SimpleBLE::BluetoothUUID char_uuid = s_characteristics[char_id];
+
+  bool result =
+      m_peripheral->notify(service_uuid, char_uuid, std::move(callback));
+  if (!result) {
+    report_error(name(), "failed to subscribe to characteristic %d", char_id);
+  }
 }
