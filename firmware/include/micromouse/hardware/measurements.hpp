f2f8b87eae2e4baa225073adbc815d12f620b42e
petelilley <hi.petelilley@gmail.com>
2026-03-05 12:55:24 -0500

    [firmware] Increase v3 track width measurement for added gears

--- firmware/include/micromouse/hardware/measurements.hpp
+++ firmware/include/micromouse/hardware/measurements.hpp
@@ -9,10 +9,17 @@ struct RobotMeasurements {
   units::millimeter_t length = 100_mm;
   units::millimeter_t width = 70_mm;
 
+  /**
+   * Distance from the center of the robot to the front and back.
+   *
+   * The center of the robot is the midpoint between the four wheels, not actually the geometric center of the
+   * robot.
+   */
   units::millimeter_t center_to_front = 55_mm;
   units::millimeter_t center_to_back = 45_mm;
 
-  units::millimeter_t track_width = 50.4_mm;
+  // Distance from the center of the left wheel to the center of the right wheel.
+  units::millimeter_t track_width = 60_mm;
 
   // Angles from the forward axis of the robot.
   units::degree_t mid_ir_sensor_angle = 45_deg;
--- firmware/platform/mouse_v3/Core/Src/hardware/measurements_impl.cpp
+++ firmware/platform/mouse_v3/Core/Src/hardware/measurements_impl.cpp
@@ -8,7 +8,7 @@ RobotMeasurements& get_robot_measurements() {
       .width = 70_mm,
       .center_to_front = 55_mm,
       .center_to_back = 45_mm,
-      .track_width = 50.4_mm,
+      .track_width = 60_mm,
       .mid_ir_sensor_angle = 45_deg,
       .far_ir_sensor_angle = 5_deg,
   };
