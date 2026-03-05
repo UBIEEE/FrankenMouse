8d701cea9a6d6ae58723eb1fff73450091c0c0cf
petelilley <hi.petelilley@gmail.com>
2026-03-05 12:56:32 -0500

    [firmware] Fix simulation feedback subscribers+publishers

--- firmware/platform/simulation/include/simulation/hardware/feedback_impl.hpp
+++ firmware/platform/simulation/include/simulation/hardware/feedback_impl.hpp
@@ -11,30 +11,22 @@
 #include <std_msgs/msg/u_int8_multi_array.hpp>
 
 class FeedbackImpl : public hardware::Feedback, public rclcpp::Node {
-  rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr m_main_task_sub;
+  rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr m_main_task_sub;
   rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr m_main_command_sub;
   rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr m_main_song_sub;
-  rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr
-      m_drive_pid_sub;
-  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr
-      m_drive_chassis_speeds_sub;
+  rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr m_drive_pid_sub;
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr m_drive_chassis_speeds_sub;
 
-  rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr m_main_task_pub;
+  rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr m_main_task_pub;
   rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr m_main_error_pub;
   rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr m_main_song_pub;
   rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr m_main_status_pub;
-  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr
-      m_vision_raw_readings_pub;
-  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr
-      m_vision_distances_pub;
-  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr
-      m_drive_motor_data_pub;
-  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr
-      m_drive_imu_data_pub;
-  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr
-      m_drive_pid_data_pub;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr
-      m_drive_chassis_speeds_pub;
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr m_vision_raw_readings_pub;
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr m_vision_distances_pub;
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr m_drive_motor_data_pub;
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr m_drive_imu_data_pub;
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr m_drive_pid_data_pub;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr m_drive_chassis_speeds_pub;
   rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr m_maze_cell_pub;
   rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr m_maze_coordinates_pub;
 
--- firmware/platform/simulation/src/hardware/feedback_impl.cpp
+++ firmware/platform/simulation/src/hardware/feedback_impl.cpp
@@ -8,87 +8,63 @@ using namespace std::placeholders;
 using namespace robot;
 
 FeedbackImpl::FeedbackImpl() : Node("micromouse_feedback") {
-  m_main_task_sub = this->create_subscription<std_msgs::msg::UInt8>(
-      "/client/main/task", 10, [this](const std_msgs::msg::UInt8& msg) {
-        Robot::get().delegate_received_feedback(feedback::TopicReceive::MAIN_TASK,
-                                                &msg.data);
+  m_main_task_sub = this->create_subscription<std_msgs::msg::UInt8MultiArray>(
+      "/client/main/task", 10, [this](const std_msgs::msg::UInt8MultiArray& msg) {
+        Robot::get().delegate_received_feedback(feedback::TopicReceive::MAIN_TASK, msg.data.data());
       });
 
   m_main_command_sub = this->create_subscription<std_msgs::msg::UInt8>(
       "/client/main/command", 10, [this](const std_msgs::msg::UInt8& msg) {
-        Robot::get().delegate_received_feedback(
-            feedback::TopicReceive::MAIN_COMMAND, &msg.data);
+        Robot::get().delegate_received_feedback(feedback::TopicReceive::MAIN_COMMAND, &msg.data);
       });
 
   m_main_song_sub = this->create_subscription<std_msgs::msg::UInt8>(
       "/client/main/song", 10, [this](const std_msgs::msg::UInt8& msg) {
-        Robot::get().delegate_received_feedback(feedback::TopicReceive::MAIN_SONG,
-                                                &msg.data);
+        Robot::get().delegate_received_feedback(feedback::TopicReceive::MAIN_SONG, &msg.data);
       });
 
   m_drive_pid_sub = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-      "/client/drive/pid", 10,
-      [this](const std_msgs::msg::Float32MultiArray& msg) {
+      "/client/drive/pid", 10, [this](const std_msgs::msg::Float32MultiArray& msg) {
         (void)msg;
         // No PID needed in simulation!
       });
 
-  m_drive_chassis_speeds_sub =
-      this->create_subscription<geometry_msgs::msg::Twist>(
-          "/client/drive/chassis_speeds", 10,
-          [this](const geometry_msgs::msg::Twist& msg) {
-            const float speeds[6] = {
-                static_cast<float>(msg.linear.x),
-                static_cast<float>(msg.linear.y),
-                static_cast<float>(msg.linear.z),
-                static_cast<float>(msg.angular.x),
-                static_cast<float>(msg.angular.y),
-                static_cast<float>(msg.angular.z),
-            };
+  m_drive_chassis_speeds_sub = this->create_subscription<geometry_msgs::msg::Twist>(
+      "/client/drive/chassis_speeds", 10, [this](const geometry_msgs::msg::Twist& msg) {
+        const float speeds[6] = {
+            static_cast<float>(msg.linear.x),  static_cast<float>(msg.linear.y),
+            static_cast<float>(msg.linear.z),  static_cast<float>(msg.angular.x),
+            static_cast<float>(msg.angular.y), static_cast<float>(msg.angular.z),
+        };
 
-            Robot::get().delegate_received_feedback(
-                feedback::TopicReceive::DRIVE_CHASSIS_SPEEDS,
-                reinterpret_cast<const uint8_t*>(speeds));
-          });
+        Robot::get().delegate_received_feedback(feedback::TopicReceive::DRIVE_CHASSIS_SPEEDS,
+                                                reinterpret_cast<const uint8_t*>(speeds));
+      });
 
-  m_main_task_pub =
-      this->create_publisher<std_msgs::msg::UInt8>("/robot/main/task", 10);
-  m_main_error_pub = this->create_publisher<std_msgs::msg::UInt8MultiArray>(
-      "/robot/main/error", 10);
-  m_main_song_pub =
-      this->create_publisher<std_msgs::msg::UInt8>("/robot/main/song", 10);
-  m_main_status_pub = this->create_publisher<std_msgs::msg::UInt8MultiArray>(
-      "/robot/main/status", 10);
+  m_main_task_pub = this->create_publisher<std_msgs::msg::UInt8MultiArray>("/robot/main/task", 10);
+  m_main_error_pub = this->create_publisher<std_msgs::msg::UInt8MultiArray>("/robot/main/error", 10);
+  m_main_song_pub = this->create_publisher<std_msgs::msg::UInt8>("/robot/main/song", 10);
+  m_main_status_pub = this->create_publisher<std_msgs::msg::UInt8MultiArray>("/robot/main/status", 10);
   m_vision_raw_readings_pub =
-      this->create_publisher<std_msgs::msg::Float32MultiArray>(
-          "/robot/vision/raw_readings", 10);
+      this->create_publisher<std_msgs::msg::Float32MultiArray>("/robot/vision/raw_readings", 10);
   m_vision_distances_pub =
-      this->create_publisher<std_msgs::msg::Float32MultiArray>(
-          "/robot/vision/distances", 10);
+      this->create_publisher<std_msgs::msg::Float32MultiArray>("/robot/vision/distances", 10);
   m_drive_motor_data_pub =
-      this->create_publisher<std_msgs::msg::Float32MultiArray>(
-          "/robot/drive/motors", 10);
-  m_drive_imu_data_pub =
-      this->create_publisher<std_msgs::msg::Float32MultiArray>(
-          "/robot/drive/imu", 10);
-  m_drive_pid_data_pub =
-      this->create_publisher<std_msgs::msg::Float32MultiArray>(
-          "/robot/drive/pid", 10);
+      this->create_publisher<std_msgs::msg::Float32MultiArray>("/robot/drive/motors", 10);
+  m_drive_imu_data_pub = this->create_publisher<std_msgs::msg::Float32MultiArray>("/robot/drive/imu", 10);
+  m_drive_pid_data_pub = this->create_publisher<std_msgs::msg::Float32MultiArray>("/robot/drive/pid", 10);
   m_drive_chassis_speeds_pub =
-      this->create_publisher<geometry_msgs::msg::Twist>(
-          "/robot/drive/chassis_speeds", 10);
-  m_maze_cell_pub = this->create_publisher<std_msgs::msg::UInt8MultiArray>(
-      "/robot/maze/cell", 10);
-  m_maze_coordinates_pub = this->create_publisher<std_msgs::msg::UInt8>(
-      "/robot/maze/coordinates", 10);
+      this->create_publisher<geometry_msgs::msg::Twist>("/robot/drive/chassis_speeds", 10);
+  m_maze_cell_pub = this->create_publisher<std_msgs::msg::UInt8MultiArray>("/robot/maze/cell", 10);
+  m_maze_coordinates_pub = this->create_publisher<std_msgs::msg::UInt8>("/robot/maze/coordinates", 10);
 }
 
 void FeedbackImpl::publish_topic(feedback::TopicSend topic, const uint8_t* data) {
   switch (topic) {
     using enum feedback::TopicSend;
     case MAIN_TASK: {
-      std_msgs::msg::UInt8 msg;
-      msg.data = data[0];
+      std_msgs::msg::UInt8MultiArray msg;
+      msg.data = std::vector<uint8_t>(data, data + 2);
       m_main_task_pub->publish(msg);
       break;
     }
