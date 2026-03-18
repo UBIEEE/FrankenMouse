2f8ef2db9089574a1cdcbed1a18c11bf9c6da078
petelilley <hi.petelilley@gmail.com>
2026-03-17 20:10:21 -0400

    [firmware] Calculate turn path length better by computing length of euler spiral segment. Cool stuff :D

--- firmware/include/micromouse/math.hpp
+++ firmware/include/micromouse/math.hpp
@@ -1,12 +1,31 @@
 #pragma once
 
+#include <gcem.hpp>
 #include <cmath>
 #include <limits>
 
 template <typename T>
   requires std::is_floating_point_v<T>
-constexpr bool float_equals(T x,
-                            T y,
-                            T epsilon = std::numeric_limits<T>::epsilon()) {
-  return std::abs(x - y) < epsilon;
+constexpr bool float_equals(T x, T y, T epsilon = std::numeric_limits<T>::epsilon()) {
+  return gcem::abs(x - y) <= epsilon;
+}
+
+/**
+ * Approximate the integral of f from a to b using the trapezoidal rule with n subdivisions.
+ *
+ * @param f The function to integrate.
+ * @param a The lower limit of integration.
+ * @param b The upper limit of integration.
+ * @param n The number of subdivisions to use (default is 100).
+ *
+ * @return The approximate integral of f from a to b.
+ */
+constexpr float approximate_integral(float (*f)(float), float a, float b, size_t n = 100) {
+  float h = (b - a) / n;
+  float sum = 0.5f * (f(a) + f(b));
+
+  for (size_t i = 1; i < n; ++i) {
+    sum += f(a + i * h);
+  }
+  return sum * h;
 }
