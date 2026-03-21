e52ae586ffaef9e53dccface7ecd90095a04889c
petelilley <hi.petelilley@gmail.com>
2026-03-21 15:38:06 -0400

    [app] Status topic

--- app/MicroMouseDashboard/StatusTopic.swift
+++ app/MicroMouseDashboard/StatusTopic.swift
@@ -1,6 +1,11 @@
 enum StatusTopic: UInt8, CaseIterable, Identifiable {
   case none = 0
+  case mazeWallGone
 
   var id: Self { self }
 }
   
+let StatusTopicDescriptions: [StatusTopic: String] = [
+  .none: "Invalid",
+  .mazeWallGone: "Maze wall gone",
+]
